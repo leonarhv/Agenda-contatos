ba1e5e47f9fd269dc63595055b4bef980d2ee656
Leonardo H Vieira <leohvir@gmail.com>
2020-03-15 16:33:41 -0300

    Ajustando requisições do modelo REST

--- src/main/java/com/leo/agenda/controller/ContatoController.java
+++ src/main/java/com/leo/agenda/controller/ContatoController.java
@@ -2,30 +2,30 @@ package com.leo.agenda.controller;
 
 import com.leo.agenda.dao.ContatoDao;
 import com.leo.agenda.modelo.Contato;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 public class ContatoController {
 
-    @RequestMapping("/adiciona-contato")
+    @PostMapping("/contatos")
     public void adiciona(Contato contato) {
         ContatoDao dao = new ContatoDao();
         dao.adiciona(contato);
     }
 
-    @RequestMapping("/contatos")
+    @GetMapping("/contatos")
     public ArrayList<Contato> listaContatos() {
         ContatoDao dao = new ContatoDao();
         return dao.getLista();
     }
 
-    @RequestMapping("/contatos/{id}")
+    @GetMapping("/contatos/{id}")
     public Contato mostraContato(@PathVariable long id) {
         ContatoDao dao = new ContatoDao();
         return dao.getContatoByID(id);
     }
+
+
 }
