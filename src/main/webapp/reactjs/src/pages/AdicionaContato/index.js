4f7aaa82cfc3e4abe51bed80b6c5623846d98c3a
Leonardo H Vieira <leohvir@gmail.com>
2020-03-16 18:03:40 -0300

    redirecionando após POST

--- src/main/webapp/reactjs/src/pages/AdicionaContato/index.js
+++ src/main/webapp/reactjs/src/pages/AdicionaContato/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import './style.css';
+import { Redirect } from 'react-router-dom';
 
 export default class AdicionaContato extends Component {
     constructor(props) {
@@ -8,7 +9,8 @@ export default class AdicionaContato extends Component {
         this.state = {
             nome: "",
             telefone: "",
-            facebook: ""
+            facebook: "",
+            redirect: false
         }
         this.handleChange = this.handleChange.bind(this);
     }
@@ -21,34 +23,42 @@ export default class AdicionaContato extends Component {
     }
 
     handleSubmit = (event) => {
+        const { nome, facebook, telefone } = this.state
         const qs = require('qs');
-        api.post('/contatos', qs.stringify(this.state)).then(response => {
-            this.setState({ nome: "", telefone: "", facebook: "" });
+        const params = qs.stringify({ nome, telefone, facebook });
+
+        api.post('/contatos', params).then(response => {
+            this.setState({ nome: "", telefone: "", facebook: "", redirect: true });
             console.log(response.status);
         });
+        
         event.preventDefault();
     }
 
     render() {
-    return (
-        <div className="adicionaContato">
-            <h2>Adicionar novo contato</h2>
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="nome">Nome:
-                <input type="text" name="nome" id="nome" placeholder="Insira seu nome aqui" value={this.state.nome} onChange={this.handleChange} />
-                </label>
+        if (this.state.redirect) {
+            return <Redirect to="/" />;
+        } else {
+            return (
+                <div className="adicionaContato">
+                    <h2>Adicionar novo contato</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="nome">Nome:
+                    <input type="text" name="nome" id="nome" placeholder="Insira seu nome aqui" value={this.state.nome} onChange={this.handleChange} />
+                        </label>
 
-                <label htmlFor="telefone">Telefone:
-                <input type="text" name="telefone" id="telefone" placeholder="Informe seu telefone (xx) xxxx-xxxx" value={this.state.telefone} onChange={this.handleChange} />
-                </label>
+                        <label htmlFor="telefone">Telefone:
+                    <input type="text" name="telefone" id="telefone" placeholder="Informe seu telefone (xx) xxxx-xxxx" value={this.state.telefone} onChange={this.handleChange} />
+                        </label>
 
-                <label htmlFor="facebook">Facebook:
-                <input type="text" name="facebook" id="facebook" placeholder="Informe o facebook do contato: facebook.com/[Pagina do contato aqui]" value={this.state.facebook} onChange={this.handleChange} />
-                </label>
+                        <label htmlFor="facebook">Facebook:
+                    <input type="text" name="facebook" id="facebook" placeholder="Informe o facebook do contato: facebook.com/[Pagina do contato aqui]" value={this.state.facebook} onChange={this.handleChange} />
+                        </label>
 
-                <button type="submit">Adicionar</button>
-            </form>
-        </div>
-    )
+                        <button type="submit">Adicionar</button>
+                    </form>
+                </div>
+            )
+        }
     }
 }
