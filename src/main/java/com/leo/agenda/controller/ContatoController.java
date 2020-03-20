b315329d1165d319a8321dd68e005e619f36c733
Leonardo H Vieira <leohvir@gmail.com>
2020-03-20 00:23:42 -0300

    Adicionando RequestMapping a classe para simplificar os metodos

--- src/main/java/com/leo/agenda/controller/ContatoController.java
+++ src/main/java/com/leo/agenda/controller/ContatoController.java
@@ -7,22 +7,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/contatos")
 @RestController
 public class ContatoController {
 
-    @PostMapping("/contatos")
+    @PostMapping
     public void adiciona(Contato contato) {
         ContatoDao dao = new ContatoDao();
         dao.adiciona(contato);
     }
 
-    @GetMapping("/contatos")
+    @GetMapping
     public ArrayList<Contato> listaContatos() {
         ContatoDao dao = new ContatoDao();
         return dao.getLista();
     }
 
-    @GetMapping("/contatos/{id}")
+    @GetMapping("/{id}")
     public Contato mostraContato(@PathVariable long id) {
         ContatoDao dao = new ContatoDao();
         return dao.getContatoByID(id);
